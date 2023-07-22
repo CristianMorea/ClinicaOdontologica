c5c9bbed01b14c2b517713909632065f06b3c882
Cristian <cristiandavidtoconasmorea@gmail.com>
2023-07-22 11:31:55 -0500

    FALTA AGREGAR LOS METODOS DE FECHA Y HORA A LA CLASE SITA MEDICA

--- Clinica/ClinicaDental.java
+++ Clinica/ClinicaDental.java
@@ -1,5 +1,8 @@
 package Clinica;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -9,24 +12,24 @@ import java.util.Scanner;
 public class ClinicaDental {
 	
 	    ArrayList<CitaMedica>listadoCitas = new ArrayList<>();
-		ArrayList<TrabajoLaboratorio>listadoLaboratorio = new ArrayList<>();	
-	    
+	    ArrayList<Odontologo>listaOdontologo = new ArrayList<>();
+		ArrayList<TrabajoLaboratorio>listadoLaboratorio = new ArrayList<>();	   
 		LinkedList<Gasto>listadoGastos = new LinkedList<>();
-		static HashMap<String, Pasiente> listadoPasiente = new HashMap<>();
+		 static HashMap<String, Pasiente> listadoPasiente = new HashMap<>();
 		static Scanner scanner = new Scanner(System.in);
-	public static void main(String[] args) 
-	{
+	
+		public static void main(String[] args) 
+		{
 		    agregarPasiente(listadoPasiente);
-		    System.out.println(listadoPasiente.toString());
-		
-		
-		
-		
-		
+		   System.out.println(listadoPasiente.toString());
+			
+
+		    
 		 scanner.close();
 		
 
-	}
+	
+		}
 	
 	public static Pasiente obtenerDatosPasiente()
 	{
@@ -58,13 +61,72 @@ public class ClinicaDental {
         
 	}
 
-	
-	public void registrarCita(CitaMedica cita) 
+	public static Odontologo obtenerDatosOdontologo()
 	{
-		
+		System.out.println("INGRESE LOS DATOS DEL ODONTOLOGO");
+		System.out.println("NOMBRE");
+		String nombre = scanner.nextLine();
+		System.out.println("APELLIDO");
+		String apellido = scanner.nextLine();
+		System.out.println("ESPECIALIDAD");
+		String cedula = scanner.nextLine();
+		System.out.println("ESPECIALIDAD");
+		String especialidad = scanner.nextLine();
+		System.out.println("TELEFONO");
+		String telefono = scanner.nextLine();
+		System.out.println("CORREO");
+		String correo = scanner.nextLine();
+
+		return new Odontologo(nombre, apellido, cedula, especialidad, telefono, correo);
 		
 	}
 	
+	public static void agregarOdontologo(ArrayList<Odontologo> listaOdontologo, Odontologo odontologo)
+	{
+		listaOdontologo.add(odontologo);
+	}
 	
-
+	
+	
+	
+	
+	public void registrarCitaMedica(Pasiente pasiente, Odontologo odontologo) 
+	{
+	   
+	   
+	    
+	    
+	    // Formatear la hora y la fecha como cadenas en el formato deseado
+	    DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+	    String horaActualString = horaActual.format(formatoHora);
+	    
+	    DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    String fechaActualString = fechaActual.format(formatoFecha);
+	    
+	    System.out.println("Fecha actual: " + fechaActualString);
+	    System.out.println("Hora actual: " + horaActualString);
+	    
+	    System.out.println("INGRECE LOS DATOS DE LA CITA MEDICA");
+	    System.out.println("FECHA (si desea la fecha actual, presione Enter): ");
+	    String fecha = scanner.nextLine();
+	    if (fecha.isEmpty()) {
+	        fecha = fechaActualString;
+	    }
+	    System.out.println("HORA (si desea la hora actual, presione Enter): ");
+	    String hora = scanner.nextLine();
+	    if (hora.isEmpty()) {
+	        hora = horaActualString;
+	    }
+	    
+	    // Crear la cita médica y almacenarla en la lista
+	    CitaMedica citaMedica = new CitaMedica(fecha, hora, pasiente, odontologo);
+	    listadoCitas.add(citaMedica);
+	}
+	
+	
+	
+	
+	
+	
+	
 }
